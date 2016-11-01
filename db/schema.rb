63c33c838cb5333b8245cb771c2778a28a3064b5
AdrianManteza <admanteza@mail.com>
2016-11-01 03:29:31 +0000

    generated the appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027025004) do
+ActiveRecord::Schema.define(version: 20161101031719) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer  "patient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false

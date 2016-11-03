692a24b35ec8294e390e283d3972428cbf126ae4
AdrianManteza <admanteza@mail.com>
2016-11-03 17:56:51 -0400

    added some comments on sees.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Set a default user for development mode
 patient = Patient.create(firstname: 'Adrian', lastname: 'Manteza', contact_num: '6475546266', 
               facebook_account: 'facebook.com', email: 'poltergeist10480@gmail.com',
               password: '123456', password_confirmation: '123456')

35c4f142db4a9583034751b1d2f02b1fa5641e71
AdrianManteza <admanteza@mail.com>
2016-11-03 20:43:43 -0400

    configured production.rb for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'sitepoint-devise.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://kinesio.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -103,3 +103,5 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 end
+
+

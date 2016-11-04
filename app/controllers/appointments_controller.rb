58313efdd7aaae6ad0c8a4e7d44248b170b2ea1e
AdrianManteza <admanteza@mail.com>
2016-11-04 13:54:54 -0400

    setup initial views for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,28 @@
 class AppointmentsController < ApplicationController
   before_action :authenticate_patient!
 
-  def new
-    
+  def index
+    @appointments = Appointment.all
   end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    
+    if @appointment.errors.any?
+      render :new
+    else
+      redirect_to appointments_path, notice: "Appointment was booked successfully"
+    end
+  end
+
+  protected
+
+  def appointment_params
+    params.require(:appointment).permit(:time_slot, :date) 
+  end
+
 end
